e0408bbe4b6fd10af0566424f8f54ac6f069d119
ArEyLiXs <anna-fisch@gmx.net>
2021-10-28 23:50:01 +0200

    Alphabet

--- FUN-app/src/app/app.component.ts
+++ FUN-app/src/app/app.component.ts
@@ -7,16 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FUN-app';
-  counter: number[];
-  index: number;
+  i: number = 1;
+  alphabet: {
+    num: number,
+    letter: string
+  }[];
+  //index: number;
 
   constructor(){
-    this.index = 0;
-    this.counter = [this.index];
+    this.alphabet = [{num: 0, letter: '?'}];
   }
-
   public onClick(): void {
-    this.index = this.index + 1;
-    this.counter.push(this.index);
-  }
+    let fullAlphabet: string[] = ["?","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    if(this.i < fullAlphabet.length){
+      this.alphabet.push({num: this.i, letter: fullAlphabet[this.i]});
+      this.i = this.i + 1;
+    } else if(this.i === fullAlphabet.length){
+      this.alphabet.push({num: 204, letter: "Error"});
+      this.i = this.i + 1;
+    }
+   // this.index = this.index + 1;
+  //  this.counter.push(this.index);
+}
 }
