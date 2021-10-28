d2bc122259138e691cde80eefe028532b41d12fa
ArEyLiXs <anna-fisch@gmx.net>
2021-10-28 18:49:53 +0200

    firstLesson

--- FUN-app/src/app/app.component.ts
+++ FUN-app/src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FUN-app';
+  counter: number[];
+  index: number;
+
+  constructor(){
+    this.index = 0;
+    this.counter = [this.index];
+  }
+
+  public onClick(): void {
+    this.index = this.index + 1;
+    this.counter.push(this.index);
+  }
 }
